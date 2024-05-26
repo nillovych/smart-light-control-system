1392ab66372874b9ad8285789547a4017c0e4ac8
Danylo <danykyurkevych@gmail.com>
2024-05-26 16:20:11 +0200

    Created authentication and registration

--- smartLightControlApp/smartLightControlApp/settings.py
+++ smartLightControlApp/smartLightControlApp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'smartLightControlApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,6 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
 
 
 # Internationalization
